1d7d0efbd15fd8a1e5f877928361ad064b46b249
Brendan Annable <brendan.annable@uon.edu.au>
2014-06-25 17:11:46 +1000

    Added classifier upload fix
    Former-commit-id: 8a7c01e4a9a130041b781bc55113d20963fbf32e

--- public/app/controller/Classifier.js
+++ public/app/controller/Classifier.js
@@ -848,7 +848,7 @@ Ext.define('NU.controller.Classifier', {
 		message.setUtcTimestamp(Date.now() / 1000);
 		message.setType(API.Message.Type.LOOKUP_TABLE);
 		var lookupTable = new API.Message.LookupTable();
-		lookupTable.setTable(this.getLookup());
+		lookupTable.setTable(dcodeIO.ByteBuffer.wrap(this.getLookup().buffer));
 		lookupTable.setBitsY(this.self.LutBitsPerColorY);
 		lookupTable.setBitsCb(this.self.LutBitsPerColorCb);
 		lookupTable.setBitsCr(this.self.LutBitsPerColorCr);
