14e639f8aaac6de0b9f7ec8ca5274dd2d03c7750
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2014-12-03 18:04:05 +1100

    Added lines for the angle widget when a click event is fired.
    Former-commit-id: 94b344fcac083a1fccf04f664f6f1bba7ffc20c3

--- public/app/view/factory/angle/AngleController.js
+++ public/app/view/factory/angle/AngleController.js
@@ -1,50 +1,74 @@
 Ext.define('NU.view.factory.angle.AngleController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.Angle',
+    draw: null,
+    centre: null,
     radius: null,
+    circle: null,
     angle: null,
     onAfterRender: function (container) {
-        container.mon(container.getEl(), 'click', this.onClick, this);
         this.renderComponents(container);
     },
+    /**
+     * Renders the circle and line for the angle view.
+     *
+     * @param view The angle view.
+     */
     renderComponents: function (view) {
         // get the dimensions from the view
         var dimensions = view.getDimensions();
         var width =  dimensions.width;
         var height = dimensions.height;
-        // create the SVG drawing context
-        var draw = SVG(view.id).size(width, height).viewbox(0, 0, width, height);
-        // create the circle
-        draw.circle(width).attr({
-            fill: '#ddd'
-        });
+        // set the radius of the circle
         this.radius = width * 0.5;
-        // create the line
-        this.angle = {
-            x: this.radius,  // || value
-            y: -this.radius, // || value
-            centre: {
-                x: this.radius,
-                y: this.radius
-            },
-            line: draw.rect(2, this.radius).attr({
-                fill: '#555'
-            }).translate(this.radius)
-        }
+        this.centre =  {
+            x: this.radius,
+            y: this.radius
+        };
+        // create the SVG drawing context
+        this.draw = SVG(view.id).size(width, height).viewbox(0, 0, width, height);
+        // draw the default circle and line
+        this.circle = this.drawCircle().click(this.onClick.bind(this));
+        this.drawLine({
+            x: 0,
+            y: this.radius
+        });
+    },
+    /**
+     * Draws a circle in the default position.
+     */
+    drawCircle: function () {
+        return this.draw.circle(this.radius * 2).attr({
+            fill: '#ddd'
+        }).move(-10, 10);
+    },
+    /**
+     * Draws a line in the default position of the circle.
+     *
+     * @param endpoint The endpoint to draw the line at. This is an object that contains an x and y coordinate.
+     */
+    drawLine: function (endpoint) {
+        this.draw.path(Ext.String.format('M {0},{1} L {2},{3}', this.centre.x, this.centre.y, endpoint.x + this.centre.x, -endpoint.y + this.centre.y)).stroke({
+            color: '#555',
+            width: 2
+        });
     },
     onClick: function (event) {
+        var bound = this.circle.node.getBoundingClientRect();
+        var x = event.x - bound.left - this.centre.x;
+        var y = -(event.y - bound.top - this.centre.y);
+        console.log("(x, y) = " + x + ", " + y);
+        var length = Math.sqrt(x * x + y * y);              // calculate the length of the vector
+        var unit = {                                        // calculate the unit vector
+            x: x / length,
+            y: y / length
+        };
+        //var angle = Math.atan2(unit.y, unit.x);
         debugger;
-        var view = this.getView();
-        var centre = this.angle.centre;
-        var x = event.getX() - view.getX() - centre.x;
-        var y = event.getY() - view.getY() - centre.y;
-        var length = Math.sqrt(x * x + y * y);
-        x /= length; // = unit
-        y /= length; // = unit
-        var angle = Math.atan2(y, x);
-        var end = {
-            x: x * this.radius,
-            y: y * this.radius
-        }
+        this.drawLine({
+            x: unit.x * this.radius,
+            y: unit.y * this.radius
+        });
+
     }
 });
