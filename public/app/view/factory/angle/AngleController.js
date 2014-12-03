dd58b5a0e4b3834d61320024e0bd65ba57e9ccbd
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2014-12-03 18:41:00 +1100

    Added grouping for the circle and the lines.
    Former-commit-id: cc2fff8f4bcd04e11ce9e9bfb8eacbeff88f6e1f

--- public/app/view/factory/angle/AngleController.js
+++ public/app/view/factory/angle/AngleController.js
@@ -4,8 +4,7 @@ Ext.define('NU.view.factory.angle.AngleController', {
     draw: null,
     centre: null,
     radius: null,
-    circle: null,
-    angle: null,
+    widget: null,
     onAfterRender: function (container) {
         this.renderComponents(container);
     },
@@ -21,14 +20,17 @@ Ext.define('NU.view.factory.angle.AngleController', {
         var height = dimensions.height;
         // set the radius of the circle
         this.radius = width * 0.5;
+        // calculate the centre of the circle
         this.centre =  {
             x: this.radius,
             y: this.radius
         };
         // create the SVG drawing context
         this.draw = SVG(view.id).size(width, height).viewbox(0, 0, width, height);
+        // create the drawing group
+        this.widget = this.draw.group();
         // draw the default circle and line
-        this.circle = this.drawCircle().click(this.onClick.bind(this));
+        this.drawCircle().click(this.onClick.bind(this));
         this.drawLine({
             x: 0,
             y: this.radius
@@ -38,9 +40,11 @@ Ext.define('NU.view.factory.angle.AngleController', {
      * Draws a circle in the default position.
      */
     drawCircle: function () {
-        return this.draw.circle(this.radius * 2).attr({
-            fill: '#ddd'
-        }).move(-10, 10);
+        var circle = this.draw.circle(this.radius * 2).fill({
+            color: '#ddd'
+        });
+        this.widget.add(circle);
+        return circle;
     },
     /**
      * Draws a line in the default position of the circle.
@@ -48,13 +52,17 @@ Ext.define('NU.view.factory.angle.AngleController', {
      * @param endpoint The endpoint to draw the line at. This is an object that contains an x and y coordinate.
      */
     drawLine: function (endpoint) {
-        this.draw.path(Ext.String.format('M {0},{1} L {2},{3}', this.centre.x, this.centre.y, endpoint.x + this.centre.x, -endpoint.y + this.centre.y)).stroke({
+        // create the line
+        var line = this.draw.path(Ext.String.format('M {0},{1} L {2},{3}', this.centre.x, this.centre.y,
+                endpoint.x + this.centre.x, -endpoint.y + this.centre.y)).stroke({
             color: '#555',
             width: 2
         });
+        this.widget.add(line);
+        return line;
     },
     onClick: function (event) {
-        var bound = this.circle.node.getBoundingClientRect();
+        var bound = this.widget.node.getBoundingClientRect();
         var x = event.x - bound.left - this.centre.x;
         var y = -(event.y - bound.top - this.centre.y);
         console.log("(x, y) = " + x + ", " + y);
@@ -64,7 +72,6 @@ Ext.define('NU.view.factory.angle.AngleController', {
             y: y / length
         };
         //var angle = Math.atan2(unit.y, unit.x);
-        debugger;
         this.drawLine({
             x: unit.x * this.radius,
             y: unit.y * this.radius
