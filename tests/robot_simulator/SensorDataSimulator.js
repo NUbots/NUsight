5198e80a873e097caf9feaea70dcaeaa34b8b26e
Brendan Annable <Brendan Annable>
2015-11-22 02:24:20 +1100

    Add randomly moving servo data to sensor data simulator

--- tests/robot_simulator/SensorDataSimulator.js
+++ tests/robot_simulator/SensorDataSimulator.js
@@ -5,19 +5,47 @@ function SensorDataSimulator () {
 	RobotSimulator.call(this);
 
 	this.loadProto('messages.input.proto.Sensors');
+
+	this.message = new this.API.messages.input.proto.Sensors({
+		timestamp: Date.now(),
+		voltage: 12,
+		battery: Math.random(),
+		servo: this.createServos()
+	});
 }
 util.inherits(SensorDataSimulator, RobotSimulator);
 
-SensorDataSimulator.prototype.run = function () {
-	var message = new this.API.messages.input.proto.Sensors({
-		timestamp: Date.now(),
-		voltage: 12,
-		battery: Math.random()
-	});
+SensorDataSimulator.prototype.createServos = function () {
+	var servos = [];
+	for (var id = 0; id < 20; id++) {
+		servos.push(this.createServo(id));
+	}
+	return servos;
+};
 
-	this.sendMessage(message);
+SensorDataSimulator.prototype.createServo = function (id) {
+	return {
+		errorFlags: 0,
+		id: id
+	}
+};
+
+SensorDataSimulator.prototype.run = function () {
+	this.message.getServo().forEach(function (servo) {
+
+		if (servo.__clockwise__ === undefined) {
+			servo.__clockwise__ = true;
+		}
+
+		if (Math.random() < 0.05) {
+			servo.__clockwise__ = !servo.__clockwise__;
+		}
+
+		servo.setPresentPosition(servo.getPresentPosition() + (servo.__clockwise__ ? 1 : -1) * Math.PI / 180);
+	}, this);
+	this.sendMessage(this.message);
 };
 
 if (require.main === module) {
-	new SensorDataSimulator().runEvery(5000);
+	new SensorDataSimulator().runEvery(50);
 }
