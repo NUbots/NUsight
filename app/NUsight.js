6e36b515d9607123f3f1d9e7484da516478421cd
Luke Farrawell <luke.farrawell@uon.edu.au>
2017-07-15 18:16:55 +1000

    Removed unused function

--- app/NUsight.js
+++ app/NUsight.js
@@ -106,23 +106,6 @@ function NUsight (io) {
 			var recordings = this.recordings;
 			// Add a callback function for this
 			client.listeners[messageType] = function (packet) {
-
-				function getByteLen(normal_val) {
-					// Force string type
-					normal_val = String(normal_val);
-
-					var byteLen = 0;
-					for (var i = 0; i < normal_val.length; i++) {
-						var c = normal_val.charCodeAt(i);
-						byteLen += c < (1 <<  7) ? 1 :
-								c < (1 << 11) ? 2 :
-								c < (1 << 16) ? 3 :
-								c < (1 << 21) ? 4 :
-								c < (1 << 26) ? 5 :
-								c < (1 << 31) ? 6 : Number.NaN;
-					}
-					return byteLen;
-				}
 				var source = packet.peer;
 				var data = packet.payload;
 
