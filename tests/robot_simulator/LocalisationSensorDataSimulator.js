ea877d662024d94d038e070cfe940ad97af7216c
Luke Farrawell <lukefarrawell@hotmail.com>
2017-07-26 10:11:21 +1000

    Fixed Localisation Sensor Data Simulator

--- tests/robot_simulator/LocalisationSensorDataSimulator.js
+++ tests/robot_simulator/LocalisationSensorDataSimulator.js
@@ -1,10 +1,13 @@
-var NUClearNet = require('nuclearnet.js');
+var NUClearNet = require('nuclearnet.js').NUClearNet;
 var LocalisationSimulator = require('./LocalisationSimulator');
 var SensorDataSimulator = require('./SensorDataSimulator');
 
 if (require.main === module) {
-	var net = new NUClearNet('LocalisationSensorDataSimulator', '239.226.152.162', 7447);
-
+	var net = new NUClearNet();
+	this.net.connect({
+		name: 'LocalisationSensorSimulator',
+		address: '10.1.255.255' // TODO: (Josephus/Trent) Remove hardcoded address after RoboCup 2017
+	});
 	new LocalisationSimulator({
 		net: net
 	}).runEvery(30);
